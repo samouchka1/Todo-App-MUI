a6cad3a738114f1505bae9417bd8d771317b3d97
samouchka1 <mchio421@protonmail.com>
2022-09-02 10:49:57 -0400

    1st phase
    under development

--- src/App.js
+++ src/App.js
@@ -1,12 +1,74 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {
+  Container,
+  Typography,
+  Box,
+  Button,
+} from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
 import './App.css';
 
-function App() {
+const containerStyles = {
+  margin: '5rem auto',
+  textAlign: 'center',
+  padding: '25px',
+  bgcolor: '#c6d8ff',
+  borderRadius: '5px',
+  border: 'solid .5px #aaaaaa',
+  boxShadow: '2px 2px 4px #c4c4c4'
+}
+
+
+const App = () => {
+
+  const [tasks, setTasks] = useState([
+      {
+          title: "Grab some Pizza",
+          completed: true
+      },
+      {
+          title: "Do your workout",
+          completed: true
+      },
+      {
+          title: "Hangout with friends",
+          completed: false
+      }
+  ]);
+
+  const Task = ({ task }) => {
+    return (
+        <div
+            className="task"
+            style={{ textDecoration: task.completed ? "line-through" : "" }}
+        >
+            {task.title}
+        </div>
+    );
+  }
+
   return (
-    <div className="App">
-      testing this app now
-    </div>
-  );
+
+    <Container maxWidth={'xs'} sx={containerStyles}>
+      <Typography variant="h4" sx={{margin: '1.5rem 0'}}>
+        Todo App!
+      </Typography>
+
+      {tasks.map((task, index) => (
+        <Box sx={{display: 'flex', justifyContent: 'space-between', padding: '1rem 3rem'}}>
+          <Task
+              task={task}
+              index={index}
+              key={index}
+          />
+          <Button variant="outlined">
+            <DeleteIcon />
+          </Button>
+        </Box>
+      ))}
+      
+    </Container>
+  )
 }
 
 export default App;
